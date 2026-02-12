d74a5326ca011959d3f1899a2599a4d006152356
FelipeCR77 <demoraesgomesfelipe@gmail.com>
2026-02-11 21:22:44 -0300

    primeiro código do curso

---
+++ Felipe.cs
@@ -0,0 +1,13 @@
+using System; // Biblioteca do c# (system) para usar comandos console 
+
+public class program
+{
+    public static void Main ()
+    {
+        Console.WriteLine("Senai");
+    }
+}
+    
+
+
+    
