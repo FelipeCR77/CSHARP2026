23b2ec24bf6d2441a6c50dba3677f0b216b4db19
FelipeCR77 <demoraesgomesfelipe@gmail.com>
2026-02-13 19:57:37 -0300

    codigo aula 3

--- Lógica de Programação e Algoritmos/aula 2/aula 2/aula 2/Felipe.cs
+++ Lógica de Programação e Algoritmos/aula 2/aula 2/aula 2/Felipe.cs
@@ -7,7 +7,3 @@ public class program
         Console.WriteLine("Senai");
     }
 }
-    
-
-
-    
---
+++ aula 3/naoseinada.cs
@@ -0,0 +1,7 @@
+double x, y, media;
+Console.Writeline("Digite o primeiro numero:");
+x=double.Parse(Console.Readline());
+Console.Writeline("Digite o segundo numero:");
+y=double.Parse(Console.Readline());
+media = (x+y)/2.0;
+Console.Writeline("Media =  " + media);
