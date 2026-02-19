937b22bb88ad0a9c21a803136855a86e8360c103
FelipeCR77 <demoraesgomesfelipe@gmail.com>
2026-02-18 21:56:01 -0300

    codigos aula 4

---
+++ aula 4/processamento.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+//Processamento de dados
+//operadores aritmeticos
+WriteLine(1+2);
+WriteLine(2-2);//Subtracao
+WriteLine(2*3);//Multiplicacao
+WriteLine(6/3);//Divisao
+WriteLine(7%3);//Resto da divisao
+WriteLine(2*2);//Potenciacao
+WriteLine((2+2)) / (2+2); // Ordem da operacao
---
+++ aula 4/saida.cs
@@ -0,0 +1,23 @@
+//Tipos de variaveis e saida de dados 
+string nome;//Criado a variavel
+nome = "Senai Americana";//Dado do tipo string
+Console.WriteLine(nome);//Saida de dados
+Console.Write(nome);
+Console.Write(nome);
+Console.WriteLine(nome);// Com quebra de linha 
+Console.Write(nome);// Sem quebra de linha
+Console.Write(nome);
+System.Console.WriteLine(nome);// Com snipptes
+string pessoa; 
+pessoa = "clodoaldo";
+System.Console.WriteLine(pessoa);
+const string ESCOLA = "SENAI AMERICANA";//Constante, nao pode ser alterada
+System.Console.WriteLine(ESCOLA);
+Console.WriteLine("O nome do usuário é: " + pessoa);//Concatenacao
+string cesarPrezBrandao;//Camelcase;
+System.Console.WriteLine("Senai\nAmericana");
+System.Console.WriteLine("Senai\nAmericana");
+
+
+
+
