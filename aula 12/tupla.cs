82ae486e7ae27e0e3142aaab76e275e11690d688
FelipeCR77 <demoraesgomesfelipe@gmail.com>
2026-03-11 21:56:30 -0300

    aula 12 2

---
+++ aula 12/tupla.cs
@@ -0,0 +1,18 @@
+//Tubla
+var Castiel = (34,34,"Céu", "Supernatural", "B");
+
+System.Console.WriteLine($"Idade do Castiel: {Castiel.Item1}");
+System.Console.WriteLine($"Nome do filho do Castiel: {Castiel.Item2}");
+System.Console.WriteLine($"Idade do Castiel: {Castiel.Item3}");
+Castiel.Item1 = 1000;
+System.Console.WriteLine($"Idade do Castiel: {Castiel.Item1}");
+
+(int, int , int) numeros = (1, 2, 5);
+
+System.Console.WriteLine($"Numeros: {numeros.Item1}, {numeros.Item2}, {numeros.Item3}");
+
+
+
+
+
+
