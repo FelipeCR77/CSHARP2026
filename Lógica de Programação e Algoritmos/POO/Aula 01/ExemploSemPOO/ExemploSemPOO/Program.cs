3713959a4687f165e29fa5af653a7c1562de17ec
FelipeCR77 <demoraesgomesfelipe@gmail.com>
2026-03-18 19:14:16 -0300

    aula 18/03

---
+++ Lógica de Programação e Algoritmos/POO/Aula 01/ExemploSemPOO/ExemploSemPOO/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+Title = " Calculadora de traingulo";
+//Entrada de dados
+Write("Digite as medidas do Triângulo X ");
+double ax = double.Parse(ReadLine());
+Write("Digite o valor de b: ");
+double bx = double.Parse(ReadLine());
+Write("Digite o valor de c: ");
+double cx = double.Parse(ReadLine());
+
+WriteLine("Digite as medidas do Triângulo Y ");
+double ay = double.Parse(ReadLine());
+Write("Digite o valor de b: ");
+double by = double.Parse(ReadLine());
+Write("Digite o valor de c: ");
+double cy = double.Parse(ReadLine());
+//Processamento de dados
+double px = (ax + bx + cx) / 2;
+double py = (ay + by + cy) / 2;
+double areax = Math.Sqrt(px * (px - ax) * (px - bx) * (px - cx));
+double areay = Math.Sqrt(py * (py - ay) * (py - by) * (py - cy));
+
+//Saída de dados
+WriteLine($"Área do Triângulo X é de {areax:F2}");
+WriteLine($"Área do Triângulo Y é de {areay:F2}");
+
+
+if (areax > areay)
+{
+    WriteLine("Maior área é do Triângulo X");
+}
+else if (areay areax)
+{
+    WriteLine("Maior área é do Triângulo Y");
+}
+else
+{
+    WriteLine("As áreas dos triângulos são iguais");
+}
+
+ReadKey();
---
+++ Lógica de Programação e Algoritmos/POO/Aula 01/Retangulo/Retangulo/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using Retangulo;
+Retangulo x = new Retangulo();
+Retangulo y = new Retangulo();
+Retangulo z = new Retangulo();
+WriteLine("Digite as medidas do Retangulo X:");
+WriteLine("Digite o comprimento do Retangulo X:");
+
+x.base = double.Parse(ReadLine());
+Write("Digite a base do Retangulo X:");
+x.altura = double.Parse(ReadLine());
+Write("Digite o valor da altura do Retangulo X:");
+
