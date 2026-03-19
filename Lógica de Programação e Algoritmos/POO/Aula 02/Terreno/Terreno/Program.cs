b83fcba39b1f19ed18d41ccf4cc3e9a06a81a1b5
FelipeCR77 <demoraesgomesfelipe@gmail.com>
2026-03-18 21:44:08 -0300

    exemplo1

--- Lógica de Programação e Algoritmos/POO/Aula 02/Terreno/Terreno/Program.cs
+++ Lógica de Programação e Algoritmos/POO/Aula 02/Terreno/Terreno/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Terreno;
+using static System.Console;
+
+Write("Digite a largura do terreno: ");
+double largura = double.Parse(ReadLine());
+Write("Digite o comprimento do terreno: ");
+double comprimento = double.Parse(ReadLine());
+Write("Digite o preço do m² do terreno: ");
+double valor = double.Parse(ReadLine());
+
+terreno t = new terreno(largura, comprimento, valor);
+t.Saida();
+ReadKey(); 
+
---
+++ Lógica de Programação e Algoritmos/POO/Aula 02/Terreno/Terreno/terreno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terreno
+{
+    internal class terreno
+    {
+        // campos
+        public double largura, comprimento, valor;
+
+        // construtor
+        public terreno(double largura, double comprimento, double valor)
+        {
+          
+            this.largura = largura;
+            this.comprimento = comprimento;
+            this.valor = valor;
+        }
+        // métodos
+        public double area()
+        {
+            return largura * comprimento;
+        }
+    
+        public double preco()
+        {
+            return area() * valor;
+        }
+    public void Saida() 
+            {
+            Console.WriteLine($"Área do terreno: {area()}");
+            Console.WriteLine($"Preço do terreno = R$ {preco()}");
+        }
+    }
+
+
+
+}
